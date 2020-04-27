474200bbf67bd9b5879c0dfcef06a70732a4f4c0
Mattt <mattt@me.com>
2020-04-27 07:34:13 -0700

    Reorganize plugins

--- _plugins/site-build_settings_count-hook.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Jekyll::Hooks.register :site, :post_read do |site|
-  site.data['build_settings_count'] = site.data['build_settings'].values.reduce(0) do |count, nested|
-    count + nested['build_settings'].count
-  end
-end
