ee56ded98b6b46291ee703a12fe8550753541c9a
Mattt <mattt@me.com>
2020-05-05 07:42:03 -0700

    Fix navigation links and intersection observation

--- assets/js/functions/registerIntersectionObservers.js
+++ assets/js/functions/registerIntersectionObservers.js
@@ -2,7 +2,7 @@ function registerIntersectionObservers() {
   if (!!window.IntersectionObserver) {
     let callback = (entries, observer) => {
       const active = entries
-        .filter(e => e.isIntersecting)
+        .filter((e) => e.isIntersecting)
         .sort(
           (a, b) =>
             b.intersectionRect.height - a.intersectionRect.height ||
@@ -11,21 +11,25 @@ function registerIntersectionObservers() {
 
       if (active) {
         requestAnimationFrame(() => {
-          document.querySelectorAll("nav ul li").forEach(li => {
+          document.querySelectorAll("nav ul li").forEach((li) => {
             li.classList.remove("active");
           });
 
-          let li = document.querySelector(
+          let a = document.querySelector(
             `nav ul ul > li a[href="#${active.target.id}"]`
-          ).parentElement;
-          li.classList.add("active");
-          li.parentElement.parentElement.classList.add("active");
+          );
+
+          if (a) {
+            let li = a.parentElement;
+            li.classList.add("active");
+            li.parentElement.parentElement.classList.add("active");
+          }
         });
       }
     };
 
     let observer = new IntersectionObserver(callback, { threshold: 0 });
-    document.querySelectorAll("section section").forEach(section => {
+    document.querySelectorAll("section section").forEach((section) => {
       observer.observe(section);
     });
   }
