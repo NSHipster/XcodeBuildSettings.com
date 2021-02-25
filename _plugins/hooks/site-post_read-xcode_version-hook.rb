066f396be72a8ae268bcb263d31c38877125aa41
Mattt <mattt@me.com>
2021-02-25 09:17:15 -0800

    Fix regular expression for xcode_version

--- _plugins/hooks/site-post_read-xcode_version-hook.rb
+++ _plugins/hooks/site-post_read-xcode_version-hook.rb
@@ -2,5 +2,5 @@
 
 Jekyll::Hooks.register :site, :post_read do |site|
   output = `xed --version`
-  site.data['xcode_version'] = output.scan(/\d+(\.\d+){1,2}/).first
+  site.data['xcode_version'] = output.scan(/\d+(?:\.\d+){0,2}/).first
 end
